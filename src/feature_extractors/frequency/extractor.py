db8660c9b2eabf3a1673f04bd1068ab86691b131
Young Song <58712406+YoungSong99@users.noreply.github.com>
2026-03-14 18:10:30 -0500

    feat: frequency feature extraction

---
+++ src/feature_extractors/frequency/extractor.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+from .fft_features import FFTFeatureExtractor
+from .dct_features import DCTFeatureExtractor
+from .dwt_features import DWTFeatureExtractor
+
+
+class FrequencyFeatureExtractor(
+    FFTFeatureExtractor,
+    DCTFeatureExtractor,
+    DWTFeatureExtractor,
+):
+    def __init__(
+        self,
+        use_fft: bool = True,
+        use_dct: bool = True,
+        use_dwt: bool = True,
+
+    ):
+        self.use_fft = use_fft
+        self.use_dct = use_dct
+        self.use_dwt = use_dwt
+
+    def extract(self, image: np.ndarray) -> dict[str, float]:
+
+        if image.ndim == 2:
+            gray_img = image
+        elif image.ndim == 3 and image.shape[2] == 3:
+            gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        else:
+            raise ValueError("input image type not gray or color")
+
+        features = {}
+
+        if self.use_fft:
+            features.update(self._fft_stats(gray_img))
+
+        if self.use_dct:
+            features.update(self._dct_stats(gray_img))
+
+        if self.use_dwt:
+            features.update(self._dwt_stats(gray_img))
+
+        return features
