2613ea3544973c16408951c987f9568afe42174b
Young Song <58712406+YoungSong99@users.noreply.github.com>
2026-03-17 02:19:19 -0500

    update: make radial dynamic

--- src/feature_extractors/frequency/radialProfile.py
+++ src/feature_extractors/frequency/radialProfile.py
@@ -1,7 +1,7 @@
-# from https://www.astrobetter.com/blog/2010/03/03/fourier-transforms-of-images-in-python/
+# from https://www.astrobetter.com/wiki/tiki-index.php?page=python_radial_profiles
 import numpy as np
 
-def azimuthalAverage(image, center=None):
+def azimuthalAverage(image, center=None, num_bins=32):
     """
     Calculate the azimuthally averaged radial profile.
 
@@ -18,24 +18,14 @@ def azimuthalAverage(image, center=None):
         center = np.array([(x.max()-x.min())/2.0, (x.max()-x.min())/2.0])
 
     r = np.hypot(x - center[0], y - center[1])
+    r_norm = r / (r.max() + 1e-8)
+        
+    bin_idx = (r_norm * num_bins).astype(np.int32)
+    bin_idx = np.clip(bin_idx, 0, num_bins - 1)
 
-    # Get sorted radii
-    ind = np.argsort(r.flat)
-    r_sorted = r.flat[ind]
-    i_sorted = image.flat[ind]
+    sums = np.bincount(bin_idx.ravel(), weights=image.ravel(), minlength=num_bins)
+    counts = np.bincount(bin_idx.ravel(), minlength=num_bins)
 
-    # Get the integer part of the radii (bin size = 1)
-    r_int = r_sorted.astype(int)
+    radial = sums / (counts + 1e-8)
 
-    # Find all pixels that fall within each radial bin.
-    deltar = r_int[1:] - r_int[:-1]  # Assumes all radii represented
-    rind = np.where(deltar)[0]       # location of changed radius
-    nr = rind[1:] - rind[:-1]        # number of radius bin
-    
-    # Cumulative sum to figure out sums for each radius bin
-    csim = np.cumsum(i_sorted, dtype=float)
-    tbin = csim[rind[1:]] - csim[rind[:-1]]
-
-    radial_prof = tbin / nr
-
-    return radial_prof
+    return radial
